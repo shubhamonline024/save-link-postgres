945f5529860c884c9ac85c18f7ac3821daeb56a8
Shubham <shubhamonline024@gmail.com>
2025-12-30 23:19:58 +0530

    changes

--- frontend/src/axios-private/AxiosPrivate.jsx
+++ frontend/src/axios-private/AxiosPrivate.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.BASEURL,
+  baseURL: import.meta.env.VITE_BASEURL,
   timeout: 10000, // optional: request timeout in ms
   headers: {
     "Content-Type": "application/json",
